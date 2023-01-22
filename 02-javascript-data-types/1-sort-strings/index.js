edee6c193b5eb2128c2c2ec0cb19f7dd7c1992e8
Daikoziyev Bokhodir <daikoziyev.baha@gmail.com>
2023-01-22 13:41:06 +0600

    2 task

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -5,5 +5,27 @@
  * @returns {string[]}
  */
 export function sortStrings(arr, param = 'asc') {
-
+  const sortedArr = [...arr];
+  const collator = new Intl.Collator(['ru-en', 'ru', 'en'], { caseFirst: 'upper' });
+  return sortedArr.sort((a, b) => {
+    const sortAnswer = collator.compare(a, b);
+    if (sortAnswer > 0) {
+      if (param === 'asc') {
+        return 1;
+      }
+      if (param === 'desc') {
+        return -1;
+      }
+    }
+    if (sortAnswer < 0) {
+      if (param === 'asc') {
+        return -1;
+      }
+      if (param === 'desc') {
+        return 1;
+      }
+    }
+    return 0;
+  });
 }
+
