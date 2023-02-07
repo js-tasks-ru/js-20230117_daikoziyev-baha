919167ae64e2eca5a2b59b1e23e3aff7143316f6
bokhodir <daikoziyev.baha@gmail.com>
2023-02-07 16:41:57 +0600

    06-events-practice, table sort, tooltip

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,62 @@
 export default class NotificationMessage {
+  static currentElem = '';
+  constructor(targetElem = '', {duration = 2000, type = 'success'}) {
+    this.targetElem = targetElem;
+    this.duration = duration;
+    this.type = type;
+    this.render();
+  }
+  getHeader() {
+    if (this.type) {
+      return `
+                 <div class="notification-header">${this.type}</div>
+            `;
+    }
+    return '';
+  }
 
+  getBody() {
+    if (this.targetElem) {
+      return `
+                <div class="notification-body">${this.targetElem}</div>
+              `;
+    }
+    return '';
+  }
+
+  getTemplate() {
+    return `
+            <div class="notification ${this.type}" style="--value:${this.duration / 1000}s">
+                <div class="timer"></div>
+                <div class="inner-wrapper">
+                  ${this.getHeader()}
+                  ${this.getBody()}
+                </div>
+            </div>
+           `;
+  }
+  show(targetElem = document.body) {
+    if (NotificationMessage.currentElem) {
+      NotificationMessage.currentElem.style.display = 'none';
+      // NotificationMessage.currentElem = null;
+    }
+    targetElem.append(this.element);
+    NotificationMessage.currentElem = this.element;
+    setTimeout(() => {
+      this.remove();
+    }, this.duration);
+  }
+  remove() {
+    this.element.remove();
+  }
+  destroy() {
+    this.remove();
+    this.element = {};
+  }
+
+  render() {
+    const wrapper = document.createElement('div');
+    wrapper.innerHTML = this.getTemplate();
+    this.element = wrapper.firstElementChild;
+  }
 }
